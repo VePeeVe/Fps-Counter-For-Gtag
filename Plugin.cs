1a0e103a284fb9761f968f09335a476dd1c09073
ve <105993367+VePeeVe@users.noreply.github.com>
2022-09-09 14:52:57 -0600

    Source Code

---
+++ Plugin.cs
@@ -0,0 +1,78 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using Utilla;
+
+namespace FpsCounter
+{
+    /// <summary>
+    /// This is your mod's main class.
+    /// </summary>
+
+    /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public float updateInterval = 0.1f; //How often should the number update
+
+        float accum = 0.0f;
+        int frames = 0;
+        float timeleft;
+        float fps;
+
+        GUIStyle textStyle = new GUIStyle();
+
+        bool inRoom;
+
+        void OnEnable()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
+
+        void OnDisable()
+        {
+            HarmonyPatches.RemoveHarmonyPatches();
+            Utilla.Events.GameInitialized -= OnGameInitialized;
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            timeleft = updateInterval;
+
+            textStyle.fontStyle = FontStyle.Bold;
+            textStyle.normal.textColor = Color.white;
+        }
+
+        void Update()
+        {
+            timeleft -= Time.deltaTime;
+            accum += Time.timeScale / Time.deltaTime;
+            ++frames;
+
+            if (timeleft <= 0.0)
+            {
+                fps = (accum / frames);
+                timeleft = updateInterval;
+                accum = 0.0f;
+                frames = 0;
+            }
+        }
+        void OnGUI()
+        {
+            GUI.Label(new Rect(5, 5, 100, 25), fps.ToString("F2") + "FPS", textStyle);
+        }
+            [ModdedGamemodeJoin]
+        public void OnJoin(string gamemode)
+        {
+            inRoom = true;
+        }
+        [ModdedGamemodeLeave]
+        public void OnLeave(string gamemode)
+        {
+            inRoom = false;
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,13 @@
+﻿
+namespace FpsCounter
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    class PluginInfo
+    {
+        public const string GUID = "com.ve.gorillatag.fpscounter";
+        public const string Name = "FpsCounter";
+        public const string Version = "1.0.0";
+    }
+}
